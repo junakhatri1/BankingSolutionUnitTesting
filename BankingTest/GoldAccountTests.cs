457057a6b94a50fafaeebf36cae99292df0ba269
Juna <junakhatri@gmail.com>
2020-04-15 14:14:44 +0000

    about to do gold account

---
+++ BankingTest/GoldAccountTests.cs
@@ -0,0 +1,25 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTest
+{
+   public class GoldAccountTests
+    {
+       
+        [Fact]
+        public void GoldAccountsGetABonusOnDeposits()
+        {
+            //Given 
+            var account = new BankAccount();
+            var originalBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+            //When
+            account.Deposit(amountToDeposit);
+            //Then
+            Assert.Equal(originalBalance + (amountToDeposit * 1.10M), account.GetBalance());
+        }
+    }
+}
